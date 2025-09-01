5d3493102994d1cf947cd4644f1feb47950a20e3
Simone Rizzo <pyro2monkey@gmail.com>
2025-09-01 23:24:21 +0200

    feat: refactor layout to remove errors, loginLogoutButton now correctly renders on authStateChange

--- app/common/LoginLogoutButton.tsx
+++ app/common/LoginLogoutButton.tsx
@@ -2,15 +2,17 @@
 import { Button } from "@/components/ui/button";
 import { signout } from "@/lib/auth-action";
 import { createClient } from "@/utils/supabase/supabaseClient";
-import { useRouter } from "next/navigation";
+import { usePathname, useRouter } from "next/navigation";
+import path from "path";
 import React, { useEffect, useState } from "react";
 
 export default function LoginLogoutButton() {
   const [user, setUser] = useState<any>(null);
   const router = useRouter();
+  const pathname = usePathname();
   useEffect(() => {
+    const supabase = createClient();
     const fetchUser = async () => {
-      const supabase = createClient();
       const {
         data: { user },
       } = await supabase.auth.getUser();
@@ -18,10 +20,22 @@ export default function LoginLogoutButton() {
       console.log("User:", user);
     };
     fetchUser();
-  }, []);
+    const { data: listener } = supabase.auth.onAuthStateChange(
+      (_event, session) => {
+        console.log("Auth state changed:", _event, session);
+        setUser(session?.user ?? null);
+      }
+    );
+    return () => {
+      listener?.subscription.unsubscribe();
+    };
+  }, [pathname]);
+
   if (user) {
     return (
       <Button
+        variant="ghost"
+        className="rounded hover:bg-gray-800 hover:text-white transition"
         onClick={() => {
           signout();
           setUser(null);
@@ -33,6 +47,8 @@ export default function LoginLogoutButton() {
   }
   return (
     <Button
+      variant="ghost"
+      className="rounded hover:bg-gray-800 hover:text-white transition"
       onClick={() => {
         router.push("/login");
       }}
